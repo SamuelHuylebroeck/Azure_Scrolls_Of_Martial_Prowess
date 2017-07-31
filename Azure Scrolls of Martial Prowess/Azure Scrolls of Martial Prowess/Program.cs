21ade5cce0a00dc5398f36bb3aa0fc5c5a7dd297
Samuel Huylebroeck <samuel.huylebroeck@hotmail.com>
2017-07-31 18:01:10 +0200

    Project creation

---
+++ Azure Scrolls of Martial Prowess/Azure Scrolls of Martial Prowess/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Azure_Scrolls_of_Martial_Prowess
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
